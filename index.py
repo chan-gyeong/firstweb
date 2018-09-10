5796772a9621ebe05a3efe2d75863684883b79e3
안찬경 <42137152+chan-gyeong@users.noreply.github.com>
2018-09-10 22:53:12 +0900

    글목록 구현
    파일 목록을 이용하여 글목록을 구현하였다.

--- index.py
+++ index.py
@@ -1,35 +1,35 @@
 #!C:\Program Files (x86)\Python37-32\python.exe
 print("content-type: text/html")
-print()
-import cgi
-import sys
-import codecs
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-form = cgi.FieldStorage()
+print()#위의 것들은 파이썬cgi를 위해 필요한 것이다.
+import cgi, sys, codecs, os
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())#한글처리
+form = cgi.FieldStorage()#page id 값 받음
+files=os.listdir('data')#파일 목록을 리스트 형태로 받음
+listStr=''
+for value in files:
+    listStr=listStr+'<li><a href="index.py?id={name}">{name}</a></li>'.format(name=value)
 if 'id' in form:
     pageId = form["id"].value
-    description = open('data/'+pageId, 'r', encoding='utf8').read()#'open()'함수 3번째 인자로 인코딩을 명시해 준다.
+    description = open('data/'+pageId, 'r', encoding='utf8').read()#'open()'함수는 3번째 인자로 인코딩을 명시해 준다.
 else :
     pageId='home'
     description = '환영합니다.'
+
 print('''<!DOCTYPE html>
 <html>
   <head>
-    <title>dkscksrud</title>
+    <title>의미없지만 의미있는 사이트</title>
     <meta charset="utf-8">
   </head>
   <body>
     <header>
-      <h1><a href="index.py">Web</a></h1>
+      <h1><a href="index.py">WEB</a></h1>
     </header>
     <ul>
-        <li><a href="index.py?id=HTML">HTML</a></li>
-        <li><a href="index.py?id=client">client</a></li>
-        <li><a href="index.py?id=server">server</a></li>
-        <li><a href="index.py?id=python">python</a></li>
+        {listStr}
     </ul>
     <h2>{title}</h2>
     <p>{desc}</p>
   </body>
 </html>
-'''.format(title=pageId, desc=description))
+'''.format(title=pageId, desc=description, listStr=listStr))
