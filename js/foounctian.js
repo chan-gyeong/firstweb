b69a1b44a835f8e8435d27818c7c24737355d4dd
안찬경 <42137152+chan-gyeong@users.noreply.github.com>
2018-12-12 21:39:06 +0900

    피보나치수열 수정, 소수 1차 완성
    역사 발표도 할 겸 겸사겸사
    1차완성본

--- js/foounctian.js
+++ js/foounctian.js
@@ -1,21 +1,42 @@
 const a=document.getElementById('number'), b=document.getElementById('return');
-var num1, num2, i;
-function primnumber(N) {
-  num1=N, num2=2, i=0;
+
+function primnumber(num1) {
+  let num2=2, i=0;
   if(num1<=1){
     b.value="1 또는 자연수가 아니다."
+    return 1;
   }
   while (num1>num2) {
     if (num1%num2 == 0) {
-      b.value="합성수";
+      b.value="합성수 "+num2;
+      return 0;
     } else {
       num2++;
     }
-    console.log("^");
-  }
-  b.value="소수";
+  }b.value="소수"; return 2;
 }/*이 소스는 안찬경이 만들었다.*/
 
-function count() {
+function count(num1){
+  let num2=2, counter=0;
+  while(num1>num2){
+    if(primnumber(num2)==2){
+      counter++;
+    }
+    num2++;
+  }
+  b.value=counter;
+  return counter;
+}
 
+function nstprime(N) {
+  let num=2, counter=0;
+  if (N<1) {
+    b.value="자연수가 아닙니다. 자연수를 입력해주세요.";
+  }
+  while (counter<N) {
+    if (primnumber(num)==2) {
+      counter++;
+    }num++;
+  }num--;
+  b.value=num; return num;
 }
